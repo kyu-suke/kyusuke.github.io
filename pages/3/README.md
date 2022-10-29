a4e1841a03e2eae75e395fbf05847eb737629214
kyu-suke <kyu-suke@users.noreply.github.com>
2022-10-29 09:58:45 +0000

    deploy: 0eaa6a07fd5322ffc1be52418b0c3e8f94346889

---
+++ pages/3/README.md
@@ -0,0 +1,2 @@
+# build, deploy
+flutter run
---
+++ pages/3/lib/models/technology.dart
@@ -0,0 +1,44 @@
+import 'package:my_portfolio/utils/constants.dart';
+
+class TechnologyModel {
+  final String name;
+  final String logo;
+
+  TechnologyModel(this.name, this.logo);
+}
+
+class TechnologyConstants {
+  // static TechnologyModel python =
+  //     TechnologyModel("Python", AppConstants.pythonImage);
+  static TechnologyModel php = TechnologyModel("PHP", AppConstants.phpImage);
+  static TechnologyModel flutter =
+      TechnologyModel("Flutter", AppConstants.flutterImage);
+  // static TechnologyModel flask =
+  //     TechnologyModel("Flask", AppConstants.flaskImage);
+  static TechnologyModel firebase =
+      TechnologyModel("Firebase", AppConstants.firebaseImage);
+  // static TechnologyModel razorPay =
+  //     TechnologyModel("Razor Pay", AppConstants.razorPayImage);
+  // static TechnologyModel cPlus =
+  //     TechnologyModel("C++", AppConstants.cPlusImage);
+  static TechnologyModel javascript =
+      TechnologyModel("Javascript", AppConstants.javascriptImage);
+  static TechnologyModel swift =
+      TechnologyModel("Swift", AppConstants.swiftImage);
+  static TechnologyModel sceneKit =
+      TechnologyModel("SceneKit", AppConstants.sceneKitImage);
+  static TechnologyModel golang =
+  TechnologyModel("Golang", AppConstants.golangImage);
+
+  static List<TechnologyModel> technologyLearned = [
+    flutter,
+    swift,
+    // cPlus,
+    // python,
+    firebase,
+    golang,
+    javascript,
+    php,
+    // flask
+  ];
+}
